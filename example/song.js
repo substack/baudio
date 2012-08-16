2c8d7859f4913fe71a2aa26365b2f66219413395
James Halliday <mail@substack.net>
2012-08-16 03:07:46 -0700

    unopts

--- example/song.js
+++ example/song.js
@@ -3,7 +3,7 @@ var spawn = require('child_process').spawn;
 var aplay = spawn('aplay', [ '-r', '44k', '-c', '2', '-f', 'S16_LE' ]);
 
 var n = 0;
-baudio({ rate : 44000 }, function (t) {
+baudio(function (t) {
     var x = Math.sin(t * 262 + Math.sin(n));
     n += Math.sin(t);
     return x;
