583765ca2536d803dbb101439effa887bf9ca59b
James Halliday <mail@substack.net>
2012-08-17 02:09:45 -0700

    multi-channel with prototypes

--- index.js
+++ index.js
@@ -1,68 +1,103 @@
 var Stream = require('stream');
+var inherits = require('inherits');
 
 module.exports = function (opts, fn) {
-    var stream = new Stream;
-    stream.readable = true;
-    
     if (typeof opts === 'function') {
         fn = opts;
         opts = {};
     }
     if (!opts) opts = {};
-    var size = opts.size || 2048;
-    var rate = opts.rate || 44000;
+    var b = new B (opts);
+    if (typeof fn === 'function') b.push(fn);
+    return b;
+};
+
+function B (opts) {
+    var self = this;
+    Stream.call(self);
     
-    var t = 0, step = 0;
+    self.readable = true;
+    self.size = opts.size || 2048;
+    self.rate = opts.rate || 44000;
     
-    stream.end = function () {
-        stream.ended = true;
-    };
+    self.channels = [];
+    self.t = 0;
+    self.i = 0;
     
-    stream.destroy = function () {
-        stream.destroyed = true;
-        stream.emit('end');
-    };
-    
-    stream.pause = function () {
-        stream.paused = true;
-    };
-    
-    stream.resume = function () {
-        if (!stream.paused) return;
-        stream.paused = false;
-        stream.emit('resume');
-    };
-    
-    process.nextTick(function loop () {
-        var buf = new Buffer(size);
-        for (var i = 0; i <= size - 2; i += 2) {
-            var n = fn.call(stream, t, step);
-            step ++;
-            t += 1 / rate;
-            buf.writeInt16LE(signed(n), i);
+    process.nextTick(function () {
+        if (self.paused) {
+            self.on('resume', self.loop.bind(self));
         }
-        
-        if (stream.destroyed) {
-            // no more events
-        }
-        else if (stream.paused) {
-            stream.once('resume', function () {
-                stream.emit('data', buf);
-                process.nextTick(loop);
-            });
-        }
-        else {
-            stream.emit('data', buf);
-            if (stream.ended) stream.emit('end');
-            else process.nextTick(loop);
-        }
+        else self.loop();
     });
+}
+
+inherits(B, Stream);
+
+B.prototype.end = function () {
+    this.ended = true;
+};
+
+B.prototype.destroy = function () {
+    this.destroyed = true;
+    this.emit('end');
+};
+
+B.prototype.pause = function () {
+    this.paused = true;
+};
+
+B.prototype.resume = function () {
+    if (!this.paused) return;
+    this.paused = false;
+};
+
+B.prototype.push = function (fn) {
+    this.channels.push(fn);
+};
+
+B.prototype.loop = function () {
+    var self = this;
     
-    return stream;
+    var buf = self.tick();
+    
+    if (self.destroyed) {
+        // no more events
+    }
+    else if (self.paused) {
+        self.once('resume', function () {
+            self.emit('data', buf);
+            process.nextTick(self.loop.bind(self));
+        });
+    }
+    else {
+        self.emit('data', buf);
+        if (self.ended) self.emit('end');
+        else process.nextTick(self.loop.bind(self));
+    }
+};
+
+B.prototype.tick = function () {
+    var self = this;
+    
+    var buf = new Buffer(self.size);
+    for (var i = 0; i <= self.size - 2; i += 2) {
+        self.channels.forEach(function (ch) {
+            var n = ch.call(self, self.t, self.i);
+            buf.writeInt16LE(signed(n), i);
+        });
+        self.i ++;
+        self.t += 1 / self.rate;
+    }
+    return buf;
 };
 
 function signed (n) {
     if (isNaN(n)) return 0;
     var b = Math.pow(2, 15);
-    return Math.max(-b, Math.min(b, Math.floor((b * n) - 1)));
+    var x = n === -1 ? -1 : n % 1;
+    return n > 0
+        ? Math.floor((b * x) - 1)
+        : Math.ceil((b * x) - 1)
+    ;
 }
