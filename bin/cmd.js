89215c24a3265c6d3c16348c3c0cf8145e2086ea
James Halliday <mail@substack.net>
2014-04-04 16:07:37 -0700

    player command with duration

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,11 +2,38 @@
 var path = require('path');
 var fs = require('fs');
 var vm = require('vm');
+var minimist = require('minimist');
+var parseDuration = require('parse-duration');
 var baudio = require('../');
 
-var src = fs.readFileSync(process.argv[2], 'utf8');
+var argv = minimist(process.argv.slice(2), {
+    alias: { i: 'infile', d: 'duration' }
+});
+var file = argv.i || argv._[0];
+
+if (argv.h || argv.help) return usage(0);
+if (!file) return usage(1);
+
+var src = fs.readFileSync(file, 'utf8');
 var ctx = { Buffer: Buffer, console: console };
 var fn = vm.runInNewContext('(function(){'+src+'})()', ctx);
 
-var b = baudio(fn);
-b.play();
+var duration = /^\d+$/.test(argv.d)
+    ? parseInt(argv.d) * 1000
+    : parseDuration(argv.d || '0')
+;
+var b = baudio(function (t, i) {
+    if (duration && t * 1000 >= duration) b.end()
+    return fn(t, i);
+});
+
+if (argv.o) b.record(argv.o)
+else b.play()
+
+function usage (code) {
+    var s = fs.createReadStream(__dirname + '/usage.txt');
+    s.pipe(code ? process.stderr : process.stdout);
+    s.on('end', function () {
+        if (code) process.exit(code);
+    });
+}
