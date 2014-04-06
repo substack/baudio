31092611d8a0035edae36e724280a878bf0260c1
James Halliday <mail@substack.net>
2014-04-05 21:58:53 -0700

    simplify the api to be single channel

--- index.js
+++ index.js
@@ -8,83 +8,45 @@ module.exports = function (opts, fn) {
         opts = {};
     }
     if (!opts) opts = {};
-    var b = new B (opts);
-    if (typeof fn === 'function') b.push(fn);
+    var b = new B (opts, fn);
     return b;
 };
 
-function B (opts) {
-    var self = this;
-    Readable.call(self);
+function B (opts, fn) {
+    Readable.call(this);
     
-    self.readable = true;
-    self.rate = opts.rate || 44000;
+    this.readable = true;
+    this.rate = opts.rate || 44000;
+    this._fn = fn;
     
-    self.channels = [];
-    self.t = 0;
-    self.i = 0;
+    this.t = 0;
+    this.i = 0;
 }
 
 inherits(B, Readable);
-
-B.prototype.addChannel = function (type, fn) {
-    if (typeof type === 'function') {
-        fn = type;
-        type = 'float';
-    }
-    this.channels.push([ type, [ fn ].filter(Boolean) ]);
-};
-
-B.prototype.push = function (ix, fn) {
-    if (typeof ix === 'function') {
-        fn = ix;
-        ix = 0;
-    }
-    if (!this.channels[ix]) {
-        this.channels[ix] = [ 'float', [] ];
-    }
-    this.channels[ix][1].push(fn);
-};
 
 B.prototype._read = function (bytes) {
     if (!bytes) bytes = 4096;
     var self = this;
     
-    var buf = new Buffer(Math.floor(bytes / self.channels.length));
+    var buf = new Buffer(Math.floor(bytes));
     function clamp (x) {
         return Math.max(Math.min(x, Math.pow(2,15)-1), -Math.pow(2,15));
     }
     
     for (var i = 0; i < buf.length; i += 2) {
-        var ch = self.channels[(i / 2) % self.channels.length];
-        var t = self.t + Math.floor(i / 2) / self.rate / self.channels.length;
-        var counter = self.i + Math.floor(i / 2 / self.channels.length);
+        var t = self.t + Math.floor(i / 2) / self.rate;
+        var counter = self.i + Math.floor(i / 2);
         
-        var value = 0;
-        var n = 0;
-        for (var j = 0; j < ch[1].length; j++) {
-            var x = ch[1][j].call(self, t, counter);
-            if (!isNaN(x)) n += x;
-        }
-        n /= ch[1].length;
-        
-        if (ch[0] === 'float') {
-            value = signed(n);
-        }
-        else {
-            var b = Math.pow(2, ch[0]);
-            var x = (Math.floor(n) % b) / b * Math.pow(2, 15);
-            if (isNaN(x)) x = 0;
-            value = x;
-        }
-        
-        buf.writeInt16LE(clamp(value), i);
+        var n = this._fn.call(self, t, counter);
+        if (isNaN(n)) n = 0;
+        buf.writeInt16LE(clamp(signed(n)), i);
     }
     
     self.i += buf.length / 2;
     self.t += buf.length / 2 / self.rate;
     
-    if (!self._ended) Readable.prototype.push.call(self, buf);
+    if (!self._ended) this.push(buf);
 };
 
 B.prototype.end = function () {
@@ -105,7 +67,7 @@ function mergeArgs (opts, args) {
 
 B.prototype.play = function (opts) {
     var ps = this._spawn('play', mergeArgs(opts, {
-        'c': this.channels.length,
+        'c': 1,
         'r': this.rate,
         't': 's16'
     }).concat('-', '-q'));
@@ -115,7 +77,7 @@ B.prototype.play = function (opts) {
 
 B.prototype.record = function (file, opts) {
     var ps = this._spawn('sox', mergeArgs(opts, {
-        'c' : this.channels.length,
+        'c' : 1,
         'r' : this.rate,
         't' : 's16',
     }).concat('-', '-q', '-o', file));
